64e602855414e7b9824447bb0e6f26bdaef84d1a
Ivan Tzikandelov <ivantzikandelov@Ivans-MacBook-Pro.local>
2014-03-18 16:35:04 -0700

    Modified for Events

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ var ip_addr = '127.0.0.1';
 var port 	= '8080';
 
 var server = restify.createServer({
-	name: "TzikiWS"
+	name: "LinkWS"
 });
 
-server.use(restify.queryParser()); // the parser content will be always available in req.query
+// the parser content will be always available in req.query
+server.use(restify.queryParser());
 
-server.use(restify.bodyParser()); // it turns the request data into a JavaScript object on the server automatically.
+//The bodyParser turns the request data into a JavaScript object on the server automatically.
+server.use(restify.bodyParser()); 
 
 // Configure Cross-origin resource sharing CORS support in the application.
 // CORS) is a mechanism that allows JavaScript on a web page to make 
@@ -23,38 +25,36 @@ server.use(restify.bodyParser()); // it turns the request data into a JavaScript
 // to determine whether or not to allow the cross-origin request.[2] It 
 // is more useful than only allowing same-origin requests, but it is 
 // more secure than simply allowing all such cross-origin requests.
-
 server.use(restify.CORS()); 
 
-var connection_string = '127.0.0.1:27017/TzikiWS';
-var db = mongojs(connection_string, ['TzikiWS']);
-var jobs = db.collection("jobs");
+var connection_string = '127.0.0.1:27017/LinkWS';
+var db = mongojs(connection_string, ['LinkWS']);
+var events = db.collection("events");
 
 // routes
-var PATH = '/jobs'
-server.get({path:PATH, version:'0.0.1'}, findAllJobs);
-server.get({path:PATH + '/:jobId', version : '0.0.1'}, findJob);
-server.post({path:PATH, version:'0.0.1'}, postNewJob);
-server.del({path:PATH +'/:jobId', version: '0.0.1'}, deleteJob);
+var PATH = '/events'
+server.get({path:PATH, version:'0.0.1'}, findAllEvents);
+server.get({path:PATH + '/:eventId', version : '0.0.1'}, findEvent);
+server.post({path:PATH, version:'0.0.1'}, postNewEvent);
+server.del({path:PATH +'/:eventId', version: '0.0.1'}, deleteEvent);
 
 
-function findAllJobs(req, res, next) {
+function findAllEvents(req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	jobs.find().limit(20).sort({postedOn : -1}, function(err, success) {
+	events.find().limit(20).sort({postedOn : -1}, function(err, success) {
 		console.log('Response success ' + success);
 		console.log('Response error ' + err);
 		if(success) {
 			res.send(200, success);
 			return next();
-		} else {
-			return next(err);
-		}
+		} 
+		return next(err);
 	});
 }
 
-function findJob(req, res, next) {
+function findEvent(req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	jobs.findOne({_id:mongojs.ObjectId(req.params.jobId)}, function(err, success) {
+	events.findOne({_id:mongojs.ObjectId(req.params.eventId)}, function(err, success) {
 		console.log('Response success ' + success);
 		console.log('Response error ' + err);
 		if(success) {
@@ -65,32 +65,30 @@ function findJob(req, res, next) {
 	});
 }
 
-
-function postNewJob(req, res, next) {
-	var job = {};
-	job.title = req.params.title;
-	console.log('Title is ' + job.title);
-	job.description = req.params.description;
-	job.location = req.params.location;
-	job.postedOn = new Date();
+function postNewEvent(req, res, next) {
+	var event = {};
+	event.title = req.params.title;
+	console.log('Title is ' + event.title);
+	event.description = req.params.description;
+	event.location = req.params.location;
+	event.postedOn = new Date();
 	
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	
-	jobs.save(job, function(err, success) {
+	events.save(event, function(err, success) {
 		console.log('Response success ' + success);
 		console.log('Response error ' + err);
 		if(success) {
-			res.send(201, job);
+			res.send(201, event);
 			return next();
 		}
 		return next(err);
 	});
 }
 
-
-function deleteJob(req, res, next) {
+function deleteEvent(req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	jobs.remove({_id:mongojs.ObjectId(req.params.jobId)}, function(err, success) {
+	events.remove({_id:mongojs.ObjectId(req.params.eventId)}, function(err, success) {
 		console.log('Response success ' + success);
 		console.log('Response error ' + err);
 		if(success) {
@@ -100,11 +98,6 @@ function deleteJob(req, res, next) {
 		return next(err);	
 	});
 }
-
-
-
-
-
 
 server.listen(port, ip_addr, function() {
 	console.log('%s listening at %s ', server.name, server.url);
